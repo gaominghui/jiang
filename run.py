918a4230303fec839eac840a620265a887cad7bb
gao.minghui <gao.minghui@immomo.com>
2017-03-16 10:36:35 +0800

    fix bug by gmh 20170316

--- run.py
+++ run.py
@@ -7,15 +7,14 @@ import time
 import os
 
 
-
+now_date = time.strftime("%Y-%m-%d", time.localtime())
+if  not os.path.exists("/Users/gaominghui/git/jiang_log/"+now_date):
+    os.mkdir("/Users/gaominghui/git/jiang_log/"+now_date)
+f = open("/Users/gaominghui/git/jiang_log/"+now_date+"/log.txt",'w')
+f.close()
+    
 for k in range(3,7):
     training_data, validation_data, test_data = load_jiang.jiang2("/Users/gaominghui/git/jiang/history.xls",k)
-    now_date = time.strftime("%Y-%m-%d", time.localtime())
-    if  not os.path.exists("/Users/gaominghui/git/jiang_log/"+now_date):
-        os.mkdir("/Users/gaominghui/git/jiang_log/"+now_date)
-    f = open("/Users/gaominghui/git/jiang_log/"+now_date+"/log.txt",'w')
-    f.close()
-    
     for i in range(50,34*k/2,10):
         net = network.Network([34 * k, i, 34],now_date)
         net.SGD(training_data, 300, 8, 1.2, test_data=test_data)
